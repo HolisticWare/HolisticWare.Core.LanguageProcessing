e65c286c1ac4b3430d4d4c8d74a0186ee8349855
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-04-28 16:38:08 +0200

    Token

---
+++ source/Core.LanguageProcessing/Token.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core.LanguageProcessing
+{
+	/// <summary>
+	/// Token.
+	/// </summary>
+	/// <see cref="https://en.wikipedia.org/wiki/Lexical_analysis#Token"/> 
+	public partial class Token<LexemeType>
+	{
+		public Token ()
+		{
+		}
+
+		public Lexeme<LexemeType> Lexeme
+		{
+			get;
+			set;
+		}
+
+	}
+}
+
